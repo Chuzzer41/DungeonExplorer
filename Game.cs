18ec4711ec4bf43863ad290746f5777a12757dc3
Charlie Lewis <charlielewis41@outlook.com>
2025-03-07 01:11:48 +0000

    Started the game class

--- Game.cs
+++ Game.cs
@@ -8,18 +8,79 @@ namespace DungeonExplorer
         private Player player;
         private Room currentRoom;
 
+        private Player Player;
+        private int RoomNumber;
+        private static Random rnd = new Random();
         public Game()
         {
-            // Initialize the game with one room and one player
-            
+            Player = new Player("", 100, new System.Collections.Generic.List<string>());
+            currentRoom = Room.RandomRoom();
+            RoomNumber = 0;
+
         }
+
+        public void PlayersDecision()
+        {
+            Console.WriteLine("Which direction would you like to go: forward, left, right?");
+
+            string input = Console.ReadLine().ToLower();
+
+            Room nextRoom;
+            switch (input)
+            {
+                case "forward":
+                    nextRoom = Room.RandomRoom();
+                    nextRoom.GetRoomDescription(Player, ref RoomNumber);
+                    break;
+
+                case "left":
+                    nextRoom = Room.RandomRoom();
+                    nextRoom.GetRoomDescription(Player, ref RoomNumber);
+                    break;
+
+                case "right":
+                    nextRoom = Room.RandomRoom();
+                    nextRoom.GetRoomDescription(Player, ref RoomNumber);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+            }
+           
+        }
+
         public void Start()
         {
-            // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            bool playing = true;
             while (playing)
             {
-                // Code your playing logic here
+
+                Console.WriteLine("Enter your name:");
+                string name = Console.ReadLine();
+                Player.Name = name;
+                player.Health = 100;
+                player.Inventory = new System.Collections.Generic.List<string>();
+
+                Console.WriteLine("Add introduction to game");
+
+                playing = false;
+                while (player.Health > 0 && RoomNumber < 10)
+                {
+                    Console.WriteLine("You are in room " + RoomNumber);
+                    PlayersDecision();
+
+
+                }
+
+                if (player.Health <= 0)
+                {
+                    Console.WriteLine("You have died.");
+                }
+                else
+                {
+                    Console.WriteLine("You have won!");
+                }
             }
         }
     }
